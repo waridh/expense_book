18adcc13662c3198e1ebbecc83445558912709bc
Bach <waridh@ualberta.ca>
2023-09-22 11:38:14 -0600

    updated the planning along with some codebase changes

---
+++ waridh-ExpBook/app/src/main/java/com/example/waridh_expbook/DetailedExpenseActivity.java
@@ -0,0 +1,20 @@
+package com.example.waridh_expbook;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailedExpenseActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- waridh-ExpBook/app/src/main/java/com/example/waridh_expbook/MainActivity.java
+++ waridh-ExpBook/app/src/main/java/com/example/waridh_expbook/MainActivity.java
@@ -5,29 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* Array of String for now */
-
+    /* Data structure for storing the expenses entries */
     private ExpenseList entries;
+
+    /* View objects that are needed for user interactions */
+    private ListView expenseListView;
+
+    /* The view adapters */
+    private ExpenseListAdapter expenseAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.entries = new ExpenseList();
+        this.entries = new ExpenseList();   // Instantiating the data structure
+        this.expenseListView = findViewById(R.id.expense_list);
+
+        /* This is some debugging entries TODO: Delete these.*/
         this.entries.add(new Expense("Bach", "2000-05", "100.00"));
         this.entries.add(new Expense("Brahm", "2000-05", "100.00"));
         this.entries.add(new Expense("Watrina", "2000-05", "1000.00"));
 
-        ExpenseListAdapter adapter = new ExpenseListAdapter(
+        this.expenseAdapter = new ExpenseListAdapter(
                 this, this.entries);
-        ListView listView = (ListView) findViewById(R.id.expense_list);
-        listView.setAdapter(adapter);
+
+        this.expenseListView.setAdapter(this.expenseAdapter);
     }
+
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            /* TODO: Put in the code that will open up the detailed view of the expense */
+        }
+    };
 
     public void openNewEntry(View view) {
         /**
@@ -36,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
          */
         Intent intent = new Intent(this, NewEntryActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * This method is used to open up the detailed view of an entry.
+     */
+    public void openEntryDetails() {
+
     }
 }
